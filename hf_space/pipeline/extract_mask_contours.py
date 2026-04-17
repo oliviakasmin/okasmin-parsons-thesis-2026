e9f95032960a7b296b5d56270e60a9299e4e1471
Olivia Kasmin <oliviakasmin@gmail.com>
2026-04-17 11:38:54 -0400

    rearrange files

--- hf_space/pipeline/extract_mask_contours.py
+++ hf_space/pipeline/extract_mask_contours.py
@@ -9,15 +9,14 @@ from PIL import Image as PImage
 test_images_dir = Path("../pipeline/test_images").resolve()
 
 
-def load_mask_image(object_id):
-    image_dir = test_images_dir
-    mask_path = image_dir / f"{object_id}_mask_standardized.png"
+def load_mask_image(object_id, image_directory):
+    mask_path = image_directory / f"{object_id}_mask_standardized.png"
     img = PImage.open(mask_path).convert("RGBA")
     return img
 
 
-def extract_outline_from_mask(object_id, alpha_threshold=8):
-    img = load_mask_image(object_id)
+def extract_outline_from_mask(object_id, alpha_threshold=8, image_directory=test_images_dir):
+    img = load_mask_image(object_id, image_directory)
     rgba = np.array(img, dtype=np.uint8)
     alpha = rgba[:, :, 3]  # best channel for masks
     _, binary = cv2.threshold(src=alpha, thresh=alpha_threshold, maxval=255, type=cv2.THRESH_BINARY)
